c462d5c8c6e4a48a140365fac338ab28cb8dcb59
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro.local>
2023-09-18 11:38:15 +1000

    boilerplate

---
+++ src/pages/ExamplePage/ExamplePage.tsx
@@ -0,0 +1,15 @@
+import reactLogo from '../../assets/react.svg'
+import viteLogo from '/vite.svg'
+import './ExamplePage.css'
+
+function ExamplePage() {
+  
+
+  return (
+    <>
+      <p>hiiiiiiiii</p>
+    </>
+  )
+}
+
+export default ExamplePage;
---
+++ src/pages/ExercisePage.tsx/ExercisePage.tsx
@@ -0,0 +1,7 @@
+export const ExercisePage = () => {
+    return(
+        <>
+            <p>peepeepoopoo</p>
+        </>
+    );
+}
