c00b7d1b14ad9ceb05ead387d0791d1f4a487343
CHUN Nicole Wai Yan <19chunnw3@Nicole-Chun-MacBookPro.local>
2023-09-27 23:05:45 +1000

    i hate css

--- src/components/ExampleComponent/ExampleComponent.tsx
+++ src/components/ExampleComponent/ExampleComponent.tsx
@@ -1,57 +1,20 @@
 import './ExampleComponent.css';
 import { Recipe } from '../../types';
 import RecipeCard from '../RecipeCard/RecipeCard';
+import { recipes } from '../../pages/ExamplePage/sampleData';
+import { useState } from 'react';
 
 const ExampleComponent = () => {
-    let cookbook:Recipe[] = [
-        {
-            name: "Spicy Meat and Seafood Fry",
-            ingredients: ["Spicy Pepper", "Hyrule Bass", "Raw Meat"],
-            health_restored: 3,
-        },
-        {
-            name: "Egg Tart",
-            ingredients: ["Bird Egg", "Goat Butter", "Cane Sugar", "Tabantha Wheat"],
-            health_restored: 2,
-        },
-        {
-            name: "Monster Curry",
-            ingredients: ["Monster Extract", "Goron Spice", "Hylian Rice"],
-            health_restored: 5,
-        },
-        {
-            name: "Hearty Meat and Rice Bowl",
-            ingredients: ["Raw Meat", "Rock Salt", "Hylian Rice"],
-            health_restored: 3,
-        },
-        {
-            name: "Salmon Risotto",
-            ingredients: ["Hearty Salmon", "Rock Salt", "Hylian Rice", "Goat Butter"],
-            health_restored: 4,
-        },
-        {
-            name: "Seafood Paella",
-            ingredients: ["Porgy", "Hearty Blueshell Snail", "Rock Salt", "Goat Butter", "Hylian Rice"],
-            health_restored: 5,
-        },
-        {
-            name: "Creamy Meat Soup",
-            ingredients: ["Any meat", "Any herb, flower, or vegetable", "Fresh Milk", "Rock Salt"],
-            health_restored: 3,
-        }
-    ]
+    const [cookbook, setCookbook] = useState<Recipe[]>(recipes);
 
     const recipesList = cookbook.map((x) => {
-        return <RecipeCard recipe={x}/>
+        return <div><RecipeCard recipe={x}/></div>
     });
 
-    return (
-        <>
-            <div id="example-component-container">
-                <h1>Link's cookbook!</h1>
-                {recipesList}
-            </div>
-        </>
+    return ( 
+        <div id="example-component-container">
+            {recipesList}
+        </div>
     )
 }
 
--- src/pages/ExamplePage/ExamplePage.tsx
+++ src/pages/ExamplePage/ExamplePage.tsx
@@ -1,39 +1,23 @@
 import { useNavigate } from 'react-router-dom';
 import './ExamplePage.css'
-import { useState } from 'react';
 import ExampleComponent from '../../components/ExampleComponent/ExampleComponent';
+import UseStateDemo from '../../components/UseStateDemo';
+import HeaderCard from '../../components/HeaderCard/HeaderCard';
 
 const ExamplePage = () => {
   const navigate = useNavigate();
 
-  // [USESTATE]
-  // NOT USESTATE -------------------
-  // This will update your number 'num', but will not show up in the frontend
-  let num = 69;
-  const updateNum = () => {
-    num = num + 1;
-    console.log("num: " + num);
-  }
-
-  // USING USESTATE -------------------
-  // can also do cool lambda function inside div!!
-  // <button onClick={() => setCount((count) => count+1)}>count is {count}</button>
-  const [count, setCount] = useState<number>(0);
-  const updateCount = () => {
-    setCount(count + 1);
-    console.log("count: " + count);
-  }
-
-  // down here you can see that 'count' is incremented on the frontend,
-  // but number always remains as 69
   return (
     <div id="example-page-container">
-      <p>wow very cool counter!!!!!! (useState good)</p>
-      <button onClick={() => updateCount()}>count is {count}</button>
-      <button onClick={() => updateNum()}>num is {num}</button>
-
-        <ExampleComponent/>
+      <div id="left-grid">
+        <HeaderCard/>
+        <UseStateDemo/>
         <button onClick={() => navigate('/')}> Go back </button>
+
+      </div>
+      <div id="right-grid">
+        <ExampleComponent/>
+      </div>
     </div>
   );
 }
